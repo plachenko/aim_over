5d085f1fc794e91d3635e1ea1dd4928a6ce858ab
Denis Perchenko <plachenko@gmail.com>
2021-04-16 17:33:11 -0400

    adding targets

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,52 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div>
+    <Target :pos="{x:0, y:0}" @on-hit="onHit(e)" />
+    <Target :pos="{x:10, y:10}" @on-hit="onHit(e)" />
+    <Target :pos="{x:30, y:30}" @on-hit="onHit(e)" />
+    <!-- <Target v-for="(target, idx) in targetArr" :key="idx" :pos="{x:0, y:0}" @on-hit="onHit(e)" /> -->
+    <div style="position: absolute; bottom: 0px;">
+      Hit: {{  }}
+
+    </div>
+    <canvas ref="can"></canvas>
+  </div>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/> -->
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import Target from './components/Target.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    Target
+  },
+  data: function(){
+    return{
+      targetArr: []
+    }
+  },
+  methods:{
+    onHit(){
+      console.log('hitted')
+    }
+  },
+  mounted: function(){
+    for(let i = 0; i< 10; i++){
+      const targetObj : {x: number, y: number, hit: boolean}[] = [{
+        x: 0, 
+        y: 0,
+        hit: false
+      }];
+
+      // this.targetArr.push(targetObj)
+    }
+
+    setInterval(()=>{
+      console.log('here');
+    }, 1000)
   }
 });
 </script>
@@ -20,8 +56,7 @@ export default defineComponent({
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  cursor: crosshair;
 }
 </style>
---
+++ src/components/Target.vue
@@ -0,0 +1,49 @@
+<template>
+  <div @click="onHit" :style="style" ref="target" class="circ"></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Target',
+  props: {
+    pos: Object,
+  },
+  computed: {
+    style: () =>{
+      // let pos = this.pos
+      const x = Math.round(Math.random()*100)
+      const y = Math.round(Math.random()*100)
+      return `top: ${x}px, left: ${y}px`
+    }
+
+  },
+  data: function(){
+    return {
+      amtHit: 0
+    }
+  },
+  methods:{
+    onHit(){
+      this.amtHit++;
+      this.$emit('on-hit')
+    }
+  },
+  mounted: function(){
+    console.log(this.pos)
+    console.log(this.$refs.target)
+  }
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.circ{
+  position: absolute;
+  height: 30px;
+  width: 30px;
+  border-radius: 30px;
+  background-color:#F00;
+}
+</style>
